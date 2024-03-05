70cc244d33977504b2dbaaaef6492bc9b731313d
Alexey Solovyev <solovyev.alexey@gmail.com>
2024-03-04 19:48:36 -0700

    Add notations to Preloaded files

--- Kata/Preloaded.lean
+++ Kata/Preloaded.lean
@@ -1,5 +1,8 @@
 def TASK_1 := ∀ n m : Nat, n + m = n + m
+notation "TASK_1" => TASK_1
 
 def TASK_2 := ∀ n m : Nat, n + m = m + n
+notation "TASK_2" => TASK_2
 
 def TASK_3 := ∀ p : Prop, p ∨ ¬p
+notation "TASK_3" => TASK_3
--- examples/calculus/Preloaded.lean
+++ examples/calculus/Preloaded.lean
@@ -5,6 +5,8 @@ import Mathlib.Analysis.SpecialFunctions.Pow.Deriv
 
 def TASK_1 := ∀ (a b x : ℝ) (f : ℝ → ℝ), (ha : a ≠ 0) →
     DifferentiableAt ℝ (fun x => f (a * x + b)) x ↔ DifferentiableAt ℝ f (a * x + b)
+notation "TASK_1" => TASK_1
 
 def TASK_2 := ∀ (x : ℝ) (f : ℝ → ℝ) (f' a : ℝ), (ha : 0 < a) → (hf : HasDerivAt f f' x) →
     HasDerivAt (fun x => a ^ f x) ((Real.log a * f') * a ^ f x) x
+notation "TASK_2" => TASK_2
