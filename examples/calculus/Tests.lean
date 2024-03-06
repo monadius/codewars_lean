e6328f8e90ab2fa1346eb28bcdccf57bd502ef83
Alexey Solovyev <solovyev.alexey@gmail.com>
2024-03-05 21:27:32 -0700

    Modify examples/calculus/Tests.lean

--- examples/calculus/Tests.lean
+++ examples/calculus/Tests.lean
@@ -1,8 +1,16 @@
 import Kata.Preloaded
 import Kata.Solution
+import Kata.CodewarsTests
 
-def task1 : TASK_1 := @DifferentiableAt.comp_linear
-#print axioms task1
+-- def task1 : TASK_1 := @DifferentiableAt.comp_linear
+-- #print axioms task1
 
-def task2 : TASK_2 := @HasDerivAt.const_rpow
-#print axioms task2
+-- def task2 : TASK_2 := @HasDerivAt.const_rpow
+-- #print axioms task2
+
+#cw_assert_type @DifferentiableAt.comp_linear : TASK_1
+#cw_assert_standard_axioms DifferentiableAt.comp_linear
+
+#cw_assert_type @HasDerivAt.const_rpow : TASK_2
+#cw_assert_standard_axioms HasDerivAt.const_rpow
+-- #cw_assert_axioms HasDerivAt.const_rpow assumes
